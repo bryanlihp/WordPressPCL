69d7bee3e1bba6d7b21d9980ad53c0567fad37c4
Bryan Li <bryan.li@burli.com>
2017-08-31 14:06:48 -0700

    added burli test user

--- WordPressPCLTests/Utility/ApiCredentials-sample.cs
+++ WordPressPCLTests/Utility/ApiCredentials-sample.cs
@@ -10,4 +10,11 @@ namespace WordPressPCLTests.Utility
         public static string Username = "Name";
         public static string Password = "password";
     }
+
+    public class ApiCredentials
+    {
+        public static string WordPressUri = "http://sburlinux01/index.php/wp-json/";
+        public static string Username = "burliadmin";
+        public static string Password = "powerbird342";
+    }
 }
